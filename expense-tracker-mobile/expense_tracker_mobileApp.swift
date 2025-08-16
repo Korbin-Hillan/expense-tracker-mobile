947d4694ef137c8ea6187f6e7b1aeb4023810426
Korbin101lee <khillan5223@gmail.com>
2025-08-16 16:55:41 -0700

    Initial Commit

---
+++ expense-tracker-mobile/expense_tracker_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_tracker_mobileApp.swift
+//  expense-tracker-mobile
+//
+//  Created by Korbin Hillan on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct expense_tracker_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expense-tracker-mobileTests/expense_tracker_mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  expense_tracker_mobileTests.swift
+//  expense-tracker-mobileTests
+//
+//  Created by Korbin Hillan on 7/13/25.
+//
+
+import Testing
+@testable import expense_tracker_mobile
+
+struct expense_tracker_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
